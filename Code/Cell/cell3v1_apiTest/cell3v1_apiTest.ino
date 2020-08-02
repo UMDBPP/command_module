1bd7fb53550248470fc41f4a2de9df03711aee64
Jonathan Molter <jonathan.molter99@gmail.com>
2020-08-02 03:27:34 -0400

    jtXBee updates

--- Code/Cell/cell3v1_apiTest/cell3v1_apiTest.ino
+++ Code/Cell/cell3v1_apiTest/cell3v1_apiTest.ino
@@ -2,10 +2,10 @@
 
 # define xbeeSerial Serial1
 
-jtXBee celltracker(xbeeSerial);
+char xbeeBuffer[100];
+const int bufSize = 100;
+jtXBee celltracker(xbeeBuffer, bufSize);
 
-
-//char xbeeBuffer[100];
 
 void setup(){
   xbeeSerial.begin(9600);
@@ -13,22 +13,39 @@ void setup(){
   
   char packetData[100] = "x?";//"131717,39.6967,-78.1957,172,test";
   char phonenumber[12] = "7324849689";
-  celltracker.txSMS(phonenumber, packetData);
+  char outputData[100];
+  memset(outputData, 0, 100);
+  celltracker.txSMS(phonenumber, packetData, outputData);
+
+  delay(1000);
+  
+  Serial.println("Init:");
+  
+  for(int i = 0; i < 100; i++){
+    //xbeeSerial.write(outputData[i]);
+  }
+
+  celltracker.nukeBuffer();
+  for(int i = 0; i < bufSize; i++){
+    //Serial.println((int)xbeeBuffer[i]);
+  }
+
+  Serial.println("EnterLoop;");
 }
 
 void loop(){
-//  if(xbeeSerial.available()){
-//    memset(xbeeBuffer,0,100);
-//    int i = 0;
-//    while(xbeeSerial.available()>0){      
-//        xbeeBuffer[i] = xbeeSerial.read();
-//        i++;
-//    }
-//
-//    for(i = 0;i<100;i++){
-//      Serial.println(int(xbeeBuffer[i]));
-//    }
-//  }
+  if(xbeeSerial.available()){
+    celltracker.nukeBuffer();
+    //int i = 0;
+    while(xbeeSerial.available()>0){      
+        //xbeeBuffer[i] = xbeeSerial.read();
+        celltracker.process(xbeeSerial.read());
+        //i++;
+      //Serial.println((int)xbeeSerial.read());
+    }
+
+    
+  }
   delay(500);
 }
 
