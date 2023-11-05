0e6cea69f6c5cf63608c19d1d6726903fc4d0f47
Michael Shipman <michael@shipman>
2023-11-04 22:17:43 -0400

    messing with RP2040 code

---
+++ Code/BITS/BITSv5/radio-test/radio_test.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <string.h>
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
+
+
+int main()
+{
+    stdio_init_all();
+    while (true)
+    {
+        printf("Hello, BITS!\n");
+        sleep_ms(1000);
+    }
+}
