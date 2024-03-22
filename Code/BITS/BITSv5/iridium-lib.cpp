c0d9716ddca8629776356308b9b9e4c5628f935f
Michael Shipman <michael@shipman>
2024-03-22 12:22:02 -0500

    first pass at iridium driver for BITSv5

---
+++ Code/BITS/BITSv5/iridium-lib.cpp
@@ -0,0 +1,80 @@
+#include "iridium-lib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hardware/uart.h"
+
+#define SBDWT_CMD "AT+SBDWT="
+#define CSQ_CMD "AT+CSQ\r"
+#define SBDIX_CMD "AT+SBDIX\r"
+#define SBDRB_CMD "AT+SBDRB\r"
+#define SBDRT_CMD "AT+SBDRT\r"
+#define CGMR_CMD "AT+CGMR\r"
+#define SBDWB_CMD "AT+SBDWB="
+
+char sbd_rx_buf[120] = {0};
+char sbd_tx_buf[120] = {0};
+
+void IridiumSBD::start_session() {
+    int mo = 0;
+    int momsn = 0;
+    int mt = 0;
+    int mtmsn = 0;
+    int mt_len = 0;
+    int mt_queued = 0;
+    char trash[10] = {0};
+
+    uart_write_blocking(uart, SBDIX_CMD, strlen(SBDIX_CMD));
+
+    read_uart_until_return();
+
+    printf("%s\n", sbd_rx_buf);
+
+    sscanf(sbd_rx_buf, "%s %d, %d, %d, %d, %d, %d", trash, mo, momsn, mt, mtmsn,
+           mt_len, mt_queued);
+
+    printf("Parsed values: %d, %d, %d, %d, %d, %d\n", mo, momsn, mt, mtmsn,
+           mt_len, mt_queued);
+}
+
+void IridiumSBD::write_SBD_text(char *data, uint len) {
+    strcpy(sbd_tx_buf, SBDWT_CMD);
+    strcat(sbd_tx_buf, data);
+    strcat(sbd_tx_buf, "\r");
+
+    uart_write_blocking(uart, (uint8_t *)sbd_tx_buf, strlen(sbd_tx_buf));
+
+    read_uart_until_return();
+
+    printf("%s\n", sbd_rx_buf);
+}
+
+void IridiumSBD::get_SBD_status() {}
+
+void IridiumSBD::read_SBD_text() {
+    uart_write_blocking(uart, SBDRT_CMD, strlen(SBDRT_CMD));
+
+    read_uart_until_return();
+
+    printf("%s\n", sbd_rx_buf);
+}
+
+void IridiumSBD::read_uart_until_return() {
+    char c = 0;
+    uint pos = 0;
+
+    c = uart_getc(uart);
+    while (c != '\n' && c != '\r') {
+        if (pos >= 119) {
+            printf("SBD read buf full\n");
+            pos = 119;
+            break;
+        }
+        sbd_rx_buf[pos] = c;
+        pos++;
+        c = uart_getc(uart);
+    }
+    sbd_rx_buf[pos] = '\0';
+}
---
+++ Code/BITS/BITSv5/iridium-lib.h
@@ -0,0 +1,57 @@
+#ifndef IRIDIUM_LIB_H
+#define IRIDIUM_LIB_H
+
+// AT+SBDIX - Initiate an Short Burst Data session, i.e. talk to the satellites.
+// Make sure you have loaded message data first. AT+SBDWT - Write text message
+// into outbound buffer AT+SBDWB - Write binary data into outbound buffer
+// AT+SBDRT - Read text message from inbound buffer
+// AT+SBDRB - Read binary data in from inbound buffer
+// AT+SBDSX - Status
+
+// Default UART settings:
+// Baud Rate = 19200
+// Data Bits = 8
+// Parity = N
+// Stop Bits = 1
+class IridiumSBD {
+   public:
+    uint rx_pin;
+    uint cts_pin;
+    uint rts_pin;
+    uint netav_pin;
+    uint ring_pin;
+    uint tx_pin;
+    uart_inst_t* uart;
+
+    IridiumSBD(uart_inst_t* p, const uint rx, const uint cts, const uint rts,
+               const uint netav, const uint ring, const uint tx) {
+        rx_pin = rx;
+        cts_pin = cts;
+        rts_pin = rts;
+        netav_pin = netav;
+        ring_pin = ring;
+        tx_pin = tx;
+        uart = p;
+
+        struct sbd_status {
+            bool out_stat;
+            int next_seq_num;
+            bool in_stat;
+            int last_seq_num;
+            bool ring_alert;
+            int msg_waiting;
+        };
+    }
+
+    void start_session(void);
+    void write_SBD_text(char* data, uint len);
+    void write_SBD_binary(void);
+    void read_SBD_text(void);
+    void read_SBD_binary(void);
+    void get_SBD_status(void);
+
+   private:
+    void read_uart_until_return(void);
+};
+
+#endif
